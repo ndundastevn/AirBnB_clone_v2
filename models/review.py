8d543bf1182c36f313c50000f715e9e3e34b35f7
Octane Mane <reachme@contact.com>
2023-08-24 04:33:18 +0300

    feat: Review model edited to allow for conenction to db

--- models/review.py
+++ models/review.py
@@ -1,10 +1,24 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
+from models.base_model import Base
 from models.base_model import BaseModel
+from sqlalchemy import Column
+from sqlalchemy import ForeignKey
+from sqlalchemy import String
+from sqlalchemy.orm import relationship
 
 
 class Review(BaseModel):
-    """ Review classto store review information """
-    place_id = ""
-    user_id = ""
-    text = ""
+    """ 
+    Review classto store review information 
+
+    Attributes:
+    __tablename__ (str): Reviews entity on db.
+    text (sqlalchemy String): review text.
+    place_id (sqlalchemy String): place id.
+    user_id (sqlalchemy String): user id.
+    """
+    __tablename__ = "reviews"
+    text = Column(String(1024), nullable=False)
+    place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
