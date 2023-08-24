bb3057d0e21bde1393e133f9a7fea09548d9ffdd
Octane Mane <reachme@contact.com>
2023-08-24 04:14:26 +0300

    feat: amenity model edited to include ORM and allow for connection to db

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,22 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from models import storage_type
+from sqlalchemy import Column, String
 
 
 class Amenity(BaseModel):
-    name = ""
+    """
+    Class represents amenity
+    Inherits from ORM Base and links to amenities entity on db.
+
+    Attributes:
+    __tablename__ (str): table on db for amenities.
+    name (sqlalchemy String): name.
+    place_amenities (sqlalchemy relationship): relationship btn
+    ammenty and place.
+    """
+    __tablename__ = "amenities"
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship("Place", secondary="place_amenity",
+                                                          viewonly=False)
