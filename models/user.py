1deb116a3d0abaf382ee5e0579c25557d3cf3c2b
Octane Mane <reachme@contact.com>
2023-08-24 05:55:30 +0300

    feat: edited to connect to db

--- models/user.py
+++ models/user.py
@@ -1,11 +1,28 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class User(BaseModel):
-    """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+    """
+    This class defines a user by various attributes
+
+    Attributes:
+    __tablename__ (str): table users on db.
+    email: (sqlalchemy String): email address.
+    password (sqlalchemy String): password.
+    first_name (sqlalchemy String): first name.
+    last_name (sqlalchemy String): last name.
+    places (sqlalchemy relationship): User-Place relationship.
+    reviews (sqlalchemy relationship): User-Review relationship.
+    """
+    __tablename__ = "users"
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
+    places = relationship("Place", backref="user", cascade="delete")
+    reviews = relationship("Review", backref="user", cascade="delete")
+
