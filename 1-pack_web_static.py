23d28ff15d637133040b764bbc0901ce32d22bd3
ndundastevn <122885990+ndundastevn@users.noreply.github.com>
2023-09-11 04:25:40 +0300

    Update 1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Fabric script generates archive from the contents of the web_static
+Fabric script generates archive from the file contents of the web_static
 """
 
 from datetime import datetime
@@ -9,7 +9,7 @@ from os.path import isdir
 
 
 def do_pack():
-    """Method to generates archive"""
+    """Method to generates archive file"""
     try:
         date = datetime.now().strftime("%Y%m%d%H%M%S")
         if isdir("versions") is False:
